d86894662869f03592cf02e8dc180379d11df8bf
Ka Hung Lee <lee.kahung414@gmail.com>
2022-12-05 15:25:04 -0800

    Completed part2e exercises 2.19 and 2.20

--- part2e-exercises/src/App.js
+++ part2e-exercises/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import personRequest from "./services/person";
+import Notification from "./components/Notification";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Numbers from "./components/Numbers";
@@ -9,6 +10,7 @@ const App = () => {
   const [query, setQuery] = useState("");
   const [newPerson, setNewPerson] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [notification, setNotification] = useState("");
 
   useEffect(() => {
     personRequest.getAll().then((returnedPersons) => {
@@ -30,6 +32,12 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const notificationTimeOut = () => {
+    setTimeout(() => {
+      setNotification("");
+    }, 5000);
+  };
+
   const handleAddPerson = (event) => {
     event.preventDefault();
     const personObject = {
@@ -46,17 +54,32 @@ const App = () => {
         const person = persons.find((p) => p.name === newPerson);
         const updatePerson = { ...person, number: newNumber };
 
-        personRequest.update(person.id, updatePerson).then((updatedPerson) => {
-          setPersons(
-            persons.map((p) => (p.name !== newPerson ? p : updatedPerson))
-          );
-        });
+        personRequest
+          .update(person.id, updatePerson)
+          .then((updatedPerson) => {
+            setPersons(
+              persons.map((p) => (p.name !== newPerson ? p : updatedPerson))
+            );
+            setNewPerson("");
+            setNewNumber("");
+            setNotification(`Updated ${updatedPerson.name}'s number`);
+            notificationTimeOut();
+          })
+          .catch((error) => {
+            setNotification(
+              `Information of ${updatePerson.name} has already been removed from server`
+            );
+            notificationTimeOut();
+            setPersons(persons.filter((n) => n.name !== updatePerson.name));
+          });
       }
     } else {
       personRequest.create(personObject).then((newPersonObject) => {
         setPersons(persons.concat(newPersonObject));
         setNewPerson("");
         setNewNumber("");
+        setNotification(`Added ${newPersonObject.name}`);
+        notificationTimeOut();
       });
     }
   };
@@ -65,13 +88,14 @@ const App = () => {
     const person = persons.find((p) => p.id === id);
     if (window.confirm(`Delete ${person.name} ?`)) {
       personRequest.remove(id);
-      setPersons(persons.filter((p) => (p.id !== id ? p : null)));
+      setPersons(persons.filter((p) => (p.id === id ? null : p)));
     }
   };
 
   return (
     <div className="App">
       <h1>Phonebook</h1>
+      <Notification notification={notification} />
       <Filter query={query} handleQuery={handleQuery} />
       <h1>add a new</h1>
       <PersonForm
