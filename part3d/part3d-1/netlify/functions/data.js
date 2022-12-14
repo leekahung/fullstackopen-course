7aa4d2b2a8bf0647e493dc43d875f495a9778072
Ka Hung Lee <lee.kahung414@gmail.com>
2022-12-13 16:41:09 -0800

    Included part3d up to Exercises 3.19 to repo

---
+++ part3d/part3d-1/netlify/functions/data.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const morgan = require("morgan");
+const Note = require("../../models/note");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/data", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+app.get("/api/data/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+app.post("/api/data", (request, response, next) => {
+  const body = request.body;
+
+  const note = new Note({
+    content: body.content,
+    date: new Date(),
+    important: body.important,
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/data/:id", (request, response, next) => {
+  const body = request.body;
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { important: body.important },
+    { new: true }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/data/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+morgan.token("body", (request) => JSON.stringify(request.body));
+const middlewareLog = ":method :url :status :res[content-length] - ms :body";
+app.use(morgan(middlewareLog));
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CaseError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
+
+const handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  const result = await handler(event, context);
+  return result;
+};
---
+++ part3d/part3d-1/src/components/Notes.js
@@ -0,0 +1,24 @@
+const Notes = ({ notes, toggleNoteImportance, handleDeleteNote }) => {
+  const listStyle = {
+    color: "grey",
+    padding: "2px",
+  };
+
+  return (
+    <ul>
+      {notes.map((note) => {
+        return (
+          <li key={note.id} style={listStyle}>
+            {note.content}{" "}
+            <button onClick={() => toggleNoteImportance(note.id)}>
+              make {note.important ? "not important" : "important"}{" "}
+            </button>{" "}
+            <button onClick={() => handleDeleteNote(note.id)}>delete</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Notes;
