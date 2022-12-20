7534e164b2c32283d90ef3c584fa1ed31697adcd
Ka Hung Lee <lee.kahung414@gmail.com>
2022-12-19 18:02:47 -0800

    Finishing part4c with section of Creating a new note and Populate

--- part4c/controllers/notes.js
+++ part4c/controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", async (_request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", {username: 1, name: 1});
   response.json(notes);
 });
 
@@ -17,11 +18,16 @@ notesRouter.get("/:id", async (request, response) => {
 
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
+
   const newNote = body.important
-    ? new Note({ ...body, date: new Date().toISOString() })
-    : new Note({ ...body, date: new Date().toISOString, important: false });
+    ? new Note({ ...body, date: new Date().toISOString(), user: user._id })
+    : new Note({ ...body, date: new Date().toISOString, important: false, user: user._id});
 
   const savedNote = await newNote.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
 
