54b3a833fc245f6481e2abc699ef092a3ed45f0e
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-12 09:59:31 -0800

    Exercise 3.1

--- part3/course-exercises/phonebook/index.js
+++ part3/course-exercises/phonebook/index.js
@@ -1,11 +1,5 @@
 const express = require("express");
-const morgan = require("morgan");
 const app = express();
-
-morgan.token("body", (request) => JSON.stringify(request.body));
-
-app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
   {
@@ -30,75 +24,11 @@ let persons = [
   },
 ];
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (_request, response) => {
   response.json(persons);
 });
-
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
-});
-
-const generateId = () => {
-  return Number(String(Math.random()).split(".")[1]);
-};
-
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
-
-  if (!body.name) {
-    return response.status(400).json({
-      error: "Missing person's name during submission",
-    });
-  } else if (!body.number) {
-    return response.status(400).json({
-      error: "Missing person's number during submission",
-    });
-  } else if (persons.find((p) => p.name === body.name) !== undefined) {
-    return response.status(400).json({
-      error: `name must be unique: the name ${body.name} already included in phonebook`,
-    });
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  };
-
-  persons = persons.concat(person);
-
-  response.json(persons);
-});
-
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((p) => p.id !== id);
-
-  response.status(204).end();
-});
-
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-    `
-  );
-});
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
-app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
