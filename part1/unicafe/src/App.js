9aa9b8891afaa23124999278922f6efa95a7fb44
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-12 01:35:06 -0800

    Exercise 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,52 @@
 import { useState } from "react";
 
+const Feedback = ({ handleFeedback }) => {
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => handleFeedback("good")}>good</button>
+      <button onClick={() => handleFeedback("neutral")}>neutral</button>
+      <button onClick={() => handleFeedback("bad")}>bad</button>
+    </div>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div></div>;
+  const handleFeedback = (feedback) => {
+    switch (feedback) {
+      case "good":
+        setGood(good + 1);
+        break;
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+      case "bad":
+        setBad(bad + 1);
+        break;
+    }
+  };
+
+  return (
+    <div>
+      <Feedback handleFeedback={handleFeedback} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
 };
 
 export default App;
