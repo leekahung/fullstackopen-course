de14344bdc89053c847193710247b892c18b3835
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-12 03:50:11 -0800

    Moving Exercises 2.1 to 2.5 to courseinfo

---
+++ part2/course-exercises/phonebook/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNewPerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+    setNewName("");
+    setPersons(persons.concat(newPerson));
+  };
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((p) => {
+        return <div key={p.name}>{p.name}</div>;
+      })}
+    </div>
+  );
+};
+
+export default App;
