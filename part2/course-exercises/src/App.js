e76c08aade52a42f8f893519c81414267d23fc10
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-12 03:36:50 -0800

    Exercise 2.5

--- part2/course-exercises/src/App.js
+++ part2/course-exercises/src/App.js
@@ -1,42 +1,4 @@
-const Header = ({ name }) => {
-  return <h3>{name}</h3>;
-};
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts
-    .map((p) => p.exercises)
-    .reduce((total, curr) => total + curr, 0);
-
-  return <strong>total of {total} exercises</strong>;
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((p) => {
-        return <Part key={p.name} name={p.name} exercises={p.exercises} />;
-      })}
-      <Total parts={parts} />
-    </div>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const Courses = ({ courses }) => {
   return (
