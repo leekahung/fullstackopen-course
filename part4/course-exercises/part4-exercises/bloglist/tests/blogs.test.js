bb57be9a6227d53610c3700f072cb70024fc4e84
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-14 18:08:58 -0800

    Exercise 4.4

--- part4/course-exercises/part4-exercises/bloglist/tests/blogs.test.js
+++ part4/course-exercises/part4-exercises/bloglist/tests/blogs.test.js
@@ -6,3 +6,53 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithMultipleBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f9",
+      title: "Bob creates house",
+      author: "Bob",
+      url: "some Url",
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f0",
+      title: "Carl",
+      author: "Carl",
+      url: "some url",
+      likes: 2,
+      __v: 0,
+    },
+  ];
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  test("when list has multiple blogs, equals sum of all likes", () => {
+    const result = listHelper.totalLikes(listWithMultipleBlog);
+    expect(result).toBe(17);
+  });
+});
--- part4/course-exercises/part4-exercises/bloglist/utils/list_helper.js
+++ part4/course-exercises/part4-exercises/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 1) {
+    return blogs[0].likes;
+  } else {
+    return blogs.map((b) => b.likes).reduce((total, curr) => total + curr, 0);
+  }
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
