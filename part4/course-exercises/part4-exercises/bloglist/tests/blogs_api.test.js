fe87ddec27581857705685f3dbc38afa4e1b061d
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-15 00:15:08 -0800

    Exercise 4.9 - Adding an initial list of blog posts with utils and beforeEach

--- part4/course-exercises/part4-exercises/bloglist/tests/blogs_api.test.js
+++ part4/course-exercises/part4-exercises/bloglist/tests/blogs_api.test.js
@@ -2,13 +2,26 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const server = require("../index");
 
+const Blog = require("../models/blog");
+const initialBlogs = require("../utils/list_helper");
+
 const api = supertest(server);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(initialBlogs);
+});
 
 test("blogs are returned as json", async () => {
   await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+test("verify unique identify property is defined as id", async () => {
+  const blogs = await api.get("/api/blogs");
+  expect(blogs.body[0].id).toBeDefined();
 });
 
 afterAll(() => {
