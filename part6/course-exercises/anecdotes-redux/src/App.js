c941f2d4d1249da3e67b30e9c1e691d28539225d
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-18 21:58:55 -0800

    Exercise 6.3

---
+++ part6/course-exercises/anecdotes-redux/src/App.js
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+import { addVote } from "./reducers/anecdoteReducer";
+
+const Anecdote = ({ anecdote, handleAddVote }) => {
+  return (
+    <div>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.vote} <button onClick={handleAddVote}>vote</button>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+
+  return (
+    <div className="App">
+      <h1>Anecdotes</h1>
+      {anecdotes.map((a) => {
+        return (
+          <Anecdote
+            key={a.id}
+            anecdote={a}
+            handleAddVote={() => dispatch(addVote(a.id))}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
---
+++ part6/course-exercises/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -0,0 +1,44 @@
+const initialAnecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
+
+const generateId = () => {
+  return Number((Math.random() * 1000000).toFixed(0));
+};
+
+const initialState = initialAnecdotes.map((a) => {
+  return {
+    content: a,
+    vote: 0,
+    id: generateId(),
+  };
+});
+
+const anecdoteReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "VOTE":
+      const anecdoteToUpdate = state.find((a) => a.id === action.data.id);
+      const updateAnecdote = {
+        ...anecdoteToUpdate,
+        vote: anecdoteToUpdate.vote + 1,
+      };
+
+      return state.map((a) => (a.id === action.data.id ? updateAnecdote : a));
+    default:
+      return state;
+  }
+};
+
+export const addVote = (id) => {
+  return {
+    type: "VOTE",
+    data: { id },
+  };
+};
+
+export default anecdoteReducer;
