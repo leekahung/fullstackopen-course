b92f16689b103191db8f6f6e58d27dbdb6907cdd
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-25 00:38:40 -0800

    Exercise 7.14

--- part7/course-exercises/bloglist/src/components/Users/Users.js
+++ part7/course-exercises/bloglist/src/components/Users/Users.js
@@ -1,10 +1,29 @@
+import { useSelector } from "react-redux";
 import Togglable from "../Togglable";
 import UserForm from "./UserForm";
 
 const Users = () => {
+  const users = useSelector((state) => state.users);
+
   return (
     <>
       <h1>Users</h1>
+      <table>
+        <tbody>
+          <th></th>
+          <th>
+            <td>blogs created</td>
+          </th>
+          {users.map((u) => {
+            return (
+              <tr>
+                <td>{u.name}</td>
+                <td>{u.blogs.length}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
       <Togglable buttonLabel="create user">
         <UserForm />
       </Togglable>
